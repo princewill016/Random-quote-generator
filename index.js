7885c3d03ee953f0418f2df97e40e63f4fe26b3f
Peter David <Princewill016@gmail.com>
2023-12-14 20:10:46 +0100

    project complete

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
 const apiKey = "5y/TFIBy5Y9TvCqrOwW49A==SW85YFhrs6KMYxsC";
 const rawCategory = [
   "age",
-  "art",
-  "birthday",
   "car",
   "courage",
-  "death",
   "dreams",
   "experience",
   "failure",
-  "faith",
   "family",
   "fear",
   "forgiveness",
-  "freedom",
   "friendship",
   "funny",
   "future",
@@ -23,7 +18,6 @@ const rawCategory = [
   "inspirational",
   "knowledge",
   "leadership",
-  "learning",
   "life",
   "love",
   "success",
@@ -32,15 +26,19 @@ const rawCategory = [
 const button = document.querySelector("button");
 button.addEventListener("click", getQuotes);
 const quotes = document.querySelector(".quotes");
+const header = document.querySelector("h1");
 const author = document.getElementById("author");
-
-setTimeout(() => {
-  window.addEventListener("load", getQuotes), 6000;
-});
+const footId1 = document.getElementById("footicon1");
+const footId2 = document.getElementById("footicon2");
+const quotesMark = document.getElementById("quotes-mark");
+const quotesMark2 = document.getElementById("quotes-mark2");
+const container = document.querySelector(".container");
+window.addEventListener("load", getQuotes);
 
 function getQuotes() {
   randomCat = Math.floor(Math.random() * rawCategory.length);
   category = rawCategory[randomCat];
+  const randomColor = chroma.random().hex();
 
   fetch(`https://api.api-ninjas.com/v1/quotes?category=${category}`, {
     method: "GET",
@@ -52,7 +50,17 @@ function getQuotes() {
     .then((response) => response.json())
     .then((result) => {
       quotes.textContent = `${result[0].quote}`;
-      author.textContent = `${result[0].author} (${result[0].category})`;
+      author.textContent = `---${result[0].author} (${result[0].category})`;
       console.log(result);
-    });
+    })
+    .catch((error) => console.error("Error fetching quotes:", error));
+  document.body.style.backgroundColor = randomColor;
+  header.style.color = randomColor;
+  button.style.backgroundColor = randomColor;
+  footId1.style.borderColor = randomColor;
+  footId1.style.color = randomColor;
+  footId2.style.borderColor = randomColor;
+  footId2.style.color = randomColor;
+  quotesMark.style.color = randomColor;
+  quotesMark2.style.color = randomColor;
 }
